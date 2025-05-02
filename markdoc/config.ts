f43f311a49bdd8012538b4a4e3672376e5a704a8
Ben Skelker <benskelker@SkelkerMac.local>
2025-05-02 14:59:35 +0300

    add vs code extension

---
+++ markdoc/config.ts
@@ -0,0 +1,11 @@
+import { callout }    from './tags';
+import { fence }      from './nodes';
+import * as functions from './functions';
+import { globals } from './vars';
+
+export default {
+  tags: { callout },
+  nodes: { fence },
+  functions,
+  variables: globals
+};
---
+++ markdoc/vars.ts
@@ -0,0 +1,11 @@
+export const globals = {
+    version: "1.0.0",
+    product: "SCA",
+    repoUrl: "https://github.com/benskelker/markdoc",
+    markdoc: {
+        "frontmatter": {
+            "title": "",
+            "name": ""
+        }
+    }
+}
